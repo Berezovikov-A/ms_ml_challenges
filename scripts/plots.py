d8c1485ec54de1436f2bb6e91ec5197872fc2a0e
ABerezovikov <alexeyberezovikov@gmail.com>
2023-01-25 00:34:12 +0600

    tydied 1 challenge

--- scripts/plots.py
+++ scripts/plots.py
@@ -1,52 +1,57 @@
 from matplotlib import pyplot as plt
+import seaborn as sns
 import numpy as np
 import pandas as pd
 from scipy import stats
 
-def show_distribution (var: pd.Series):
-    '''
-    This function accepts a pandas series and shows distribution of values
-    '''
-    #find min, max, average, median and mode:
-    min_val = var.min()
-    max_val = var.max()
-    ave_val = var.mean()
-    med_val = var.median()
-    mod_val = var.mode()[0]
 
-    #create a figure for 2 plots 2 rows 1 column size 18 by 5
-    fig, ax = plt.subplots(2,1,figsize = (18, 5))
+def show_distribution(var_data: pd.Series):
 
-    #plot histogram
-    ax[0].hist(var)
+    '''
+    Function to show summary stats and distribution for a column
+    '''
+
+    min_val = var_data.min()
+    max_val = var_data.max()
+    mean_val = var_data.mean()
+    med_val = var_data.median()
+    mod_val = var_data.mode()[0]
+
+    fig, ax = plt.subplots(2, 1, figsize = (15,6))
+
+    sns.histplot(var_data, binwidth = abs(mean_val), kde=True, ax=ax[0])
     ax[0].set_ylabel('Frequency')
 
-    #lines for min, max, average, median and mode
-    ax[0].axvline(x=min_val, color='yellow', linestyle = 'dashed', linewidth = 2)
-    ax[0].axvline(x=max_val, color='grey', linestyle = 'dashed', linewidth = 2)
-    ax[0].axvline(x=ave_val, color='red', linestyle = 'dashed', linewidth = 2)
-    ax[0].axvline(x=med_val, color='cyan', linestyle = 'dashed', linewidth = 2)
-    ax[0].axvline(x=mod_val, color='blue', linestyle = 'dashed', linewidth = 2)
+    ax[0].axvline(x=min_val, color = 'gray', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=mean_val, color = 'cyan', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=med_val, color = 'red', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=mod_val, color = 'yellow', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=max_val, color = 'gray', linestyle='dashed', linewidth = 2)
 
-    #plot the box figure
-    ax[1].boxplot(var, vert=False)
-    ax[1].set_xlabel('value')
+    sns.boxplot(x=var_data, ax=ax[1])
 
-    #set the title
-    fig.suptitle('Data Distribution for {} Minimum = {:.2f} Average = {:.2f} Median = {:.2f} Mode = {:.2f} Maximum = {:.2f}'.format(var.name,min_val, ave_val, med_val, mod_val, max_val))
+    ax[1].set_xlabel('Value')
 
-def show_box(df: pd.DataFrame, feature: str, label: str):
+    # Add a title to the Figure
+    fig.suptitle(var_data.name +': Minimum:{:.2f} | Mean:{:.2f} | Median:{:.2f} | Mode:{:.2f} | Maximum:{:.2f}'.format(min_val,
+                                                                                                                        mean_val,
+                                                                                                                        med_val,
+                                                                                                                        mod_val,
+                                                                                                                        max_val))
+
+def show_box(var_data: pd.DataFrame):
     '''
-    This function accepts a Pandas dataframe and column names for a categorical feature and label and returns a box plot to show their relationship
+    This function accepts a Pandas dataframe with a label and a feature and shows a box plot to show their relationship
     '''
-    df.boxplot(column=label, by=feature, figsize=(8,8))
-    plt.xticks(rotation=90)
+    fig = plt.figure(figsize=(10,6))
+    
+    fig = sns.boxplot(data = var_data, y = var_data.columns[0], x = var_data.columns[1])
 
 def show_correlation(label: pd.Series, feature: pd.Series):
     '''
     This function accepts 2 Pandas series and shows their correlation as a scatter plot and a line demonstrating linear regression
     '''
-    fig = plt.figure(figsize=(9,6))
+    fig = plt.figure(figsize=(9,9))
     ax = fig.gca()
     correlation = feature.corr(label)
 
