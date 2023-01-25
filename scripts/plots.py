b0b5f1e728f4fa5d307b72481ba18285427e2bf8
ABerezovikov <alexeyberezovikov@gmail.com>
2023-01-25 20:17:44 +0600

    just updating

--- scripts/plots.py
+++ scripts/plots.py
@@ -19,7 +19,7 @@ def show_distribution(var_data: pd.Series):
 
     fig, ax = plt.subplots(2, 1, figsize = (15,6))
 
-    sns.histplot(var_data, binwidth = abs(mean_val), kde=True, ax=ax[0])
+    sns.histplot(var_data, binwidth = 15, kde=True, ax=ax[0])
     ax[0].set_ylabel('Frequency')
 
     ax[0].axvline(x=min_val, color = 'gray', linestyle='dashed', linewidth = 2)
@@ -46,43 +46,3 @@ def show_box(var_data: pd.DataFrame):
     fig = plt.figure(figsize=(10,6))
     
     fig = sns.boxplot(data = var_data, y = var_data.columns[0], x = var_data.columns[1])
-
-def show_correlation(label: pd.Series, feature: pd.Series):
-    '''
-    This function accepts 2 Pandas series and shows their correlation as a scatter plot and a line demonstrating linear regression
-    '''
-    fig = plt.figure(figsize=(9,9))
-    ax = fig.gca()
-    correlation = feature.corr(label)
-
-    # regression slope and intercept
-    slope, intercept, r, p, std_err = stats.linregress(feature, label)
-    #calculate f(x)
-    df_reg = pd.Series(dtype='object')
-    df_reg ['fx']= (slope * feature) + intercept
-    
-    plt.scatter(x=feature, y=label)
-    plt.xlabel(feature.name)
-    plt.ylabel(label.name)
-    plt.title(label.name + ' vs ' + feature.name + ' - correlation:' + str(correlation) + '\nslope: {:.4f}\ny-intercept: {:.4f}'.format(slope,intercept))
-    plt.plot(feature, df_reg['fx'], color = 'red')
-
-def show_correlation_heatmap(df: pd.DataFrame):
-    '''
-    This function accepts a dataset with numeric values and transforms it to a correlation matrix in form of a heatmap
-    '''
-    corr_matrix = round(df.corr(), 2)
-
-    fig, ax = plt.subplots(figsize=(9,6))
-
-    ax.imshow(corr_matrix)
-    ax.set_xticks(range(len(corr_matrix)), corr_matrix.columns)
-    ax.set_yticks(range(len(corr_matrix)), corr_matrix.index)
-
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-         rotation_mode="anchor")
-
-    for i, ind in enumerate(corr_matrix.index):
-        for j, col in enumerate(corr_matrix.columns):
-            ax.text(j, i, corr_matrix.loc[ind, col],
-                        ha="center", va="center", color="w")
